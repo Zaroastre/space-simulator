cc4752443928f90325da6553c476ca477ee21e01
Gitea <gitea@fake.local>
2022-06-16 13:23:17 +0200

    Add missions classes

--- src/main/java/com/spie/spacesimulator/interfaces/cli/CommandLineInterface.java
+++ src/main/java/com/spie/spacesimulator/interfaces/cli/CommandLineInterface.java
@@ -8,10 +8,25 @@ import com.spie.spacesimulator.interfaces.api.UserInterface;
 
 public class CommandLineInterface implements UserInterface {
 
+    private void displayRemoteMissionControlCenterMenu() {
+        System.out.println("List satellites");
+        System.out.println("Connect to satellite");
+        System.out.println("Leave remote Mission Control Center");
+    }
+
+    private void displayMissionControlCenterMenu() {
+        System.out.println("Create a new satellite");
+        System.out.println("List local satellites");
+        System.out.println("List remotes Mission Control Centers");
+        System.out.println("Connect to local satellite");
+        System.out.println("Add remote Mission Control Center");
+        System.out.println("Connect to remote Mission Control Center");
+    }
+
     private void displayMainMenu() {
         System.out.println("Welcome on Space Simulator.");
         System.out.println("1. Create a new Mission Control Center");
-        System.out.println("2. Create a new satellite.");
+        System.out.println("1. Connect to local Mission Control Center");
         System.out.println("0. Quit the simulation.");
 
         Scanner keyboard = new Scanner(System.in);
